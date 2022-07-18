3ef64a7a8532902042e09e451aa9b22c6420e130
Antonino Scordino <git@antonino.cf>
2022-07-18 22:09:33 +0200

    sm8150-common: Build `GCamGO`

--- common.mk
+++ common.mk
@@ -163,7 +163,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
     vendor.oneplus.camera.CameraHIDL@1.0.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    GCamGO
 
 # Common init scripts
 PRODUCT_PACKAGES += \
