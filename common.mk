b326f6488fd7ef905f657c9f9cf3666c80a51791
Antonino Scordino <git@antonino.cf>
2022-07-22 13:01:11 +0200

    sm8150-common: Inherit `vendor/extras`

--- common.mk
+++ common.mk
@@ -12,6 +12,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
+# Get extra aspects
+$(call inherit-product, vendor/extras/product.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
